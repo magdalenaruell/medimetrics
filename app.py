3023253e3bb11def0868a8cddd84c81d3d4519d7
magdalenaruell <magdalena.ruell@gmx.net>
2025-02-09 22:19:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -241,11 +241,14 @@ if selected_file1 and selected_file2:
         comparison_html += "</table>"
 
         st.subheader("📊 Vergleich der Tabellen")
-        st.markdown("Gleiche Werte in beiden Tabellen = 🟩 
-        Unterschiedliche Werte in beiden Tabellen = 🟥 
-        Komplette Zeilen-Übereinstimmung = 🟢 
-        Teilweise Übereinstimmung = 🟠
-        Keine Übereinstimmung = 🔴")
+       st.markdown("""
+        **Gleiche Werte in beiden Tabellen = 🟩**  
+        **Unterschiedliche Werte in beiden Tabellen = 🟥**  
+        **Komplette Zeilen-Übereinstimmung = 🟢**  
+        **Teilweise Übereinstimmung = 🟠**  
+        **Keine Übereinstimmung = 🔴**  
+        """)
+        
         st.markdown(comparison_html, unsafe_allow_html=True)
 
     except Exception as e:
