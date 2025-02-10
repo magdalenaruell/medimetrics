6bda7112af67c64425920b263c79aed56c07e291
magdalenaruell <magdalena.ruell@gmx.net>
2025-02-10 15:50:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ st.markdown(
     position: relative;
     box-shadow: 0px 4px 15px rgba(0,0,0,0.3);
     padding-bottom: 0px;
-    margin-bottom: -100px; /* KEINE negative Margin */
+    margin-bottom: -200px; /* KEINE negative Margin */
 }
 
 /* Logo & Titel Container */
